47c9a1addc56fcca5969f441c1072482c54fad4c
Maria Grebenkova <grebenkova.m.p@gmail.com>
2021-04-05 21:44:47 +0300

    initial commit

---
+++ src/test/java/PostmanApiTest.java
@@ -0,0 +1,26 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanApiTest {
+    private final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("https://postman-echo.com")
+            .build();
+
+    @Test
+    void shouldPostedData() {
+        given()
+                .spec(requestSpec)
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .header("content-length", equalTo("423"))
+                .body("data", containsString("some data"));
+
+    }
+}
